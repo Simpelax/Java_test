4c8249f8b8253378e9b65911d6310d746ed19f25
Stepanov Vladimir <ggguuuggg43@gmail.com>
2024-05-16 07:55:52 +0300

    main method and String input with split

--- src/main/java/org/example/Test_task.java
+++ src/main/java/org/example/Test_task.java
@@ -1,8 +1,19 @@
 package org.example;
 
+import java.util.Scanner;
 public class Test_task {
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("__Calculator__");
+        System.out.println("Write your expression:");
+        Scanner s = new Scanner(System.in);
+        String input = s.nextLine();
+        String result = calc(input);
+        System.out.println("The result is: " + result);
+    }
+}
     public static String calc(String input) {
-            //starting....
+        String[] symbols = input.split(" ");
         return input;
     }
 }
