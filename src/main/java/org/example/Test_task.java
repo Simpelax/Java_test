91e3c96bf1ad3e5afa23e8b1016fa1c7a7c645ce
Stepanov Vladimir <ggguuuggg43@gmail.com>
2024-05-16 08:26:58 +0300

    fix method Calc

--- src/main/java/org/example/Test_task.java
+++ src/main/java/org/example/Test_task.java
@@ -8,12 +8,30 @@ public class Main {
         System.out.println("Write your expression:");
         Scanner s = new Scanner(System.in);
         String input = s.nextLine();
-        String result = calc(input);
+        String result = calc(input); // берём с psS calc(String input)
         System.out.println("The result is: " + result);
     }
 }
     public static String calc(String input) {
-        String[] symbols = input.split(" ");
-        return input;
+        String[] symbols = input.split(" "); // выражение в массив посимвольно и разбить пробелами на части
+        if (symbols.length != 3) {
+            System.out.println("Invalid input");
+        }
+        int firstSymbol = Integer.parseInt(symbols[0]);
+        String operation = symbols[1];
+        int secondSymbol = Integer.parseInt(symbols[2]);
+        int total = 0;
+        if (operation.equals("+")) {
+            total = firstSymbol + secondSymbol;
+        } else if (operation.equals("-")) {
+            total = firstSymbol - secondSymbol;
+        } else if (operation.equals("*")) {
+            total = firstSymbol * secondSymbol;
+        } else if (operation.equals("/")) {
+            total = firstSymbol / secondSymbol;
+        } else {
+            return "Invalid operation";
+        }
+        return String.valueOf(total);
     }
 }
