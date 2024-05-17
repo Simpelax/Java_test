2b871f6daad217c6860ae4558c6562ece1ed4602
Stepanov Vladimir <ggguuuggg43@gmail.com>
2024-05-17 13:00:11 +0300

    Exceptions successfully learned

--- src/main/java/org/example/Test_task.java
+++ src/main/java/org/example/Test_task.java
@@ -1,23 +1,25 @@
 package org.example;
 
 import java.util.Scanner;
-public class Test_task {
-public class Main {
+
+class Calculator {
+
     public static void main(String[] args) {
         System.out.println("__Calculator__");
         System.out.println("Write your expression:");
         Scanner s = new Scanner(System.in);
         String input = s.nextLine();
-        String result = calc(input); // берём с psS calc(String input)
-        System.out.println("The result is: " + result);
+        int result = calc(input);  // берём с psS calc(String input)
+        System.out.println("The result is: "+result);
     }
-}
-    public static String calc(String input) {
-        String[] symbols = input.split(" "); // выражение в массив посимвольно и разбить пробелами на части
+
+    public static int calc(String input) {
+        String[] symbols = input.split(" "); // выражение в массив посимвольно и разбить по пробелам на части
         if (symbols.length != 3) {
             System.out.println("Invalid input");
+            return 0; // проверка на 2 оператора и 1 операнд
         }
-        int firstSymbol = Integer.parseInt(symbols[0]);
+        int firstSymbol = Integer.parseInt(symbols[0]); // перевод строки на входе с консоли в числа
         String operation = symbols[1];
         int secondSymbol = Integer.parseInt(symbols[2]);
         int total = 0;
@@ -28,10 +30,15 @@ public class Main {
         } else if (operation.equals("*")) {
             total = firstSymbol * secondSymbol;
         } else if (operation.equals("/")) {
+            if (secondSymbol == 0 && firstSymbol == 0) {
+                System.out.println("Cannot divide by zero");
+                return 0; // деление на ноль
+            }
             total = firstSymbol / secondSymbol;
         } else {
-            return "Invalid operation";
+            System.out.println("Invalid operation");
+            return 0; // Return a default value as an error indicator
         }
-        return String.valueOf(total);
+        return total;
     }
 }
