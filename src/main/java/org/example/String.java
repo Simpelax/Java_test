b184a610f6b2b3d461e3e629801d485de8027242
Simpelax <72455506+Simpelax@users.noreply.github.com>
2024-05-21 08:35:10 +0300

    Delete src/main/java/org/example/String.java

--- src/main/java/org/example/String.java
+++
@@ -1,8 +0,0 @@
-  public static void main(String[] args) {
-    String m = "Hello ";
-    String myString = "Vov4ick";
-    System.out.println(m + myString);
-    System.out.println("Let's " + "do this shit " + "again.");
-  }
-
-
